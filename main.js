323eedb6ab04e9cf303e07459fa1a90d094e0568
Easyvac16 <gdima9051@gmail.com>
2024-01-20 20:56:45 +0200

    A lot of changes

--- main.js
+++ main.js
@@ -32,3 +32,17 @@ function toggleTextVisibility() {
         hiddenText.style.display = 'none';
     }
 }
+
+function showTab(tabId, tabElement) {
+    let tabs = document.getElementsByClassName('tab-content');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+    document.getElementById(tabId).classList.add('active');
+
+    let allTabs = document.getElementsByClassName('tab');
+    for (let j = 0; j < allTabs.length; j++) {
+        allTabs[j].classList.remove('active');
+    }
+    tabElement.classList.add('active');
+}
