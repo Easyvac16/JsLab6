1ab733e39392d0fd7055b7a05aabd21e598a2012
Easyvac16 <gdima9051@gmail.com>
2024-01-20 14:52:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function generateRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 101);
+
+    document.getElementById('randomNumberDisplay').innerText = `Random number: ${randomNumber}`;
+}
+
+
+function showMouseCoordinates(event) {
+    document.getElementById('coordinates').innerText = `Mouse cords: ${event.clientX} , ${event.clientY}`;
+}
+
+function showMouseButton(event) {
+    let buttonClicked = '';
+    if (event.type === 'click') {
+        if (event.button === 0) {
+            buttonClicked = 'left click';
+        }
+    } else if (event.type === 'contextmenu') {
+        event.preventDefault();
+        buttonClicked = 'Right click';
+    }
+
+    document.getElementById('mouseButton').innerText = `Mouse button:${buttonClicked} `;
+}
+
+window.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+})
+
+function toggleTextVisibility() {
+    var hiddenText = document.getElementById('hiddenText');
+
+    if (hiddenText.style.display === 'none') {
+        hiddenText.style.display = 'block';
+    } else {
+        hiddenText.style.display = 'none';
+    }
+}
